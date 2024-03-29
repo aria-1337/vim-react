4f9087e7f451f8ad8537b01df47309aa92d82996
aria-1337 <aria.lopez.dev@gmail.com>
2024-03-28 22:03:39 -0700

    Keypress tracking + basic dev display

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import UseKeyPress from './hooks/UseKeypress';
 
-function App() {
-  return (<>
-  </>);
+export default function App() {
+    // Store + Record keypress events
+    const [mem, setMem] = useState<Array<any>>([]);
+    const { char, type } = UseKeyPress();
+    useEffect(() => {
+        setMem((oldMem) => [...oldMem, { char, type }]);
+    }, [char]);
+
+
+    return (<DevContainer>
+        <VimContainer>
+        </VimContainer>
+
+        <DevDivider height="50px" />
+        <DevRow>{`Last key event => char: ${char} type: ${type}`}</DevRow>
+        <DevCol>{ mem?.reverse().map((i, x) => x > 7 ? null : <DevRow key={x}>{`${i.char} -- ${i.type}`}</DevRow>)}</DevCol>
+    </DevContainer>);
 }
 
-export default App;
+const VimContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+
+// ------------------------------------------------------------------------ Dev/Testing styling
+
+const DevDivider = styled.div<{ height: string }>`
+    min-width: 10px;
+    min-height: ${props => props.height};
+`;
+
+const DevContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const DevRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    background-color: black;
+    color: white;
+    padding: 10px;
+`;
+
+const DevCol = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
